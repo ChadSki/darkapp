16f52356d3285155488d83ba876e99c6fc5762aa
chadski <chad.zawistowski@gmail.com>
2017-12-16 14:19:31 -0800

    Import nimbus library, adjust main.py

--- src/main.py
+++ src/main.py
@@ -5,7 +5,29 @@ from PyQt5.QtWidgets import (
 from PyQt5.QtGui import QPalette as qp
 
 
-class Main(QTreeView):
+def build_palette():
+    palette = QtGui.QPalette()
+    foreground = QtCore.Qt.white
+    accent = QtGui.QColor(53, 53, 53)
+    background = QtGui.QColor(15, 15, 15)
+    for item, color in (
+            (qp.Window, accent),
+            (qp.WindowText, foreground),
+            (qp.Base, background),
+            (qp.AlternateBase, accent),
+            (qp.ToolTipBase, foreground),
+            (qp.ToolTipText, foreground),
+            (qp.Text, foreground),
+            (qp.Button, accent),
+            (qp.ButtonText, foreground),
+            (qp.BrightText, QtCore.Qt.red),
+            (qp.Highlight, accent),
+            (qp.HighlightedText, foreground)):
+        palette.setColor(item, color)
+    return palette
+
+
+class MainWindow(QTreeView):
     def __init__(self):
         QTreeView.__init__(self)
         # self.setWindowFlags(QtCore.Qt.FramelessWindowHint)
@@ -19,35 +41,16 @@ class Main(QTreeView):
         print(file_path)
 
 
-def build_palette():
-    palette = QtGui.QPalette()
-    foreground = QtCore.Qt.white
-    accent = QtGui.QColor(53, 53, 53)
-    background = QtGui.QColor(15, 15, 15)
-    highlight = QtGui.QColor(142, 45, 197).lighter()
-    highlight_foreground = QtCore.Qt.black
-    for item, color in (
-            (qp.Window, accent),
-            (qp.WindowText, foreground),
-            (qp.Base, background),
-            (qp.AlternateBase, accent),
-            (qp.ToolTipBase, foreground),
-            (qp.ToolTipText, foreground),
-            (qp.Text, foreground),
-            (qp.Button, accent),
-            (qp.ButtonText, foreground),
-            (qp.BrightText, QtCore.Qt.red),
-            (qp.Highlight, highlight),
-            (qp.HighlightedText, highlight_foreground)):
-        palette.setColor(item, color)
-    return palette
+class MainApplication(QApplication):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.setStyle('Fusion')
+        self.setPalette(build_palette())
 
 
 def main(argv):
     import sys
-    app = QtWidgets.QApplication(sys.argv)
-    app.setStyle('Fusion')
-    app.setPalette(build_palette())
-    MainWindow = Main()
-    MainWindow.show()
+    app = MainApplication(sys.argv)
+    window = MainWindow()
+    window.show()
     sys.exit(app.exec_())
